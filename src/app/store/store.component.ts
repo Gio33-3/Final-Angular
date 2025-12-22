3bba4b8950c8787ed0ba3c52c7f5dc2724fe42ad
Gio33-3 <gioele.guarnaccia14@gmail.com>
2025-12-22 11:54:32 +0100

    inizio modifiche page 2

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -7,13 +7,13 @@ import { AppleService } from '../services/apple.service';
   styleUrls: ['./store.component.scss']
 })
 export class StoreComponent implements OnInit {
-  prodotti: any[] = [];
+  products: any[] = [];
 
   constructor(private service: AppleService) { }
 
   ngOnInit() {
     this.service.getProducts().subscribe((data) => {
-      this.prodotti = data.products;
+      this.products = data.products;
     });
   }
 }
